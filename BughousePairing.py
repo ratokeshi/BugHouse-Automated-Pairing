321fe26dec6f3e3bea4a4b6609799075d8406442
Roy Tokeshi <ratokeshi@users.noreply.github.com>
2018-02-08 22:20:30 -0700

    Create BughousePairing.py

---
+++ BughousePairing.py
@@ -0,0 +1,8 @@
+variable = [2, 3, 5, 7, 11, 13, 17, 19]
+def permutations(iterable, r=None):
+    pool = tuple(iterable)
+    n = len(pool)
+    r = n if r is None else r
+    for indices in product(range(n), repeat=r):
+        if len(set(indices)) == r:
+            yield tuple(pool[i] for i in indices)
